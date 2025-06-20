ec3ac22c495a773a20724049d3635484cc27c32b
ASPCoding <152766272+ASPCoding@users.noreply.github.com>
2025-06-20 01:13:50 -0700

    Add Links

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import lemonBanner from './assets/fresh lemonaid.avif'
 import instalogo from './assets/insta.png'
 import twitterlogo from './assets/twitterlogo.png'
+import shark from './assets/bday decals blahaj.avif'
 import './App.css'
 
 function App() {
@@ -31,8 +32,25 @@ function App() {
         </div>
 
         <div id="links">
+          <h1>LemonAid</h1>
           <p>welcome :3c</p>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSedhZLyFn9VVGTj0m6f5EOSFUnoWwc8gEopbOyNZ1yutWtnBw/closedform" target="_blank" rel="noopener noreferrer">
+            <button id="commision">Commission Form</button>
+          </a>
+          <a href="google.com" target="_blank" rel="noopener noreferrer">
+            <button id="example">Example</button>
+          </a>
+          <a href="https://docs.google.com/document/d/11xHLcwYBXGbOAwcrt1eCRpIIYF6pdFbsNhYw0bDZcx0/edit?tab=t.0" target="_blank" rel="noopener noreferrer">
+            <button id="tos">Terms of Service</button>
+          </a>
+          <a href="https://www.ikea.com/us/en/p/blahaj-soft-toy-shark-90373590/" target="_blank" rel="noopener noreferrer">
+            <img src={shark}/>
+          </a>
         </div>
+      </div>
+
+      <div className="aboutme">
+        <p>hi</p>
       </div>
 
       <div className="artworks">
